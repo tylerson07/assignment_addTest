d3298aed6ea068e7723c4862c0627a9d416d434b
이건희 <leebiki@gmail.com>
2024-02-07 21:53:12 +0900

    spring security 인증 제외

--- src/main/java/com/sparta/newsfeed_project/NewsfeedProjectApplication.java
+++ src/main/java/com/sparta/newsfeed_project/NewsfeedProjectApplication.java
@@ -2,8 +2,9 @@ package com.sparta.newsfeed_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class NewsfeedProjectApplication {
 
     public static void main(String[] args) {
