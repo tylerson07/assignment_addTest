d5c0d34d6326c240b076f227a346270d8a4c1e1f
testonenyeong <testonenyeong@naver.com>
2024-02-07 19:33:56 +0900

    first commit

---
+++ src/main/java/com/sparta/newsfeed_project/NewsfeedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeed_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsfeedProjectApplication.class, args);
+    }
+
+}
