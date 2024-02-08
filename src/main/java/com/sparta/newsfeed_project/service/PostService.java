d771f503e655e425e3fc51dcfb110baed1f3d691
tyler <smooky0702@gmail.com>
2024-02-08 10:28:20 +0900

    ..

--- src/main/java/com/sparta/newsfeed_project/service/PostService.java
+++ src/main/java/com/sparta/newsfeed_project/service/PostService.java
@@ -1,4 +1,15 @@
 package com.sparta.newsfeed_project.service;
 
+import com.sparta.newsfeed_project.repository.PostRepository;
+
 public class PostService {
+    private PostRepository postRepository;
+    public PostService(PostRepository postRepository){
+        this.postRepository = postRepository;
+
+    }
+
+
+
+
 }
